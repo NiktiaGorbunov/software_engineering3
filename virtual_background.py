839074e83c0f526b481de0b922e0230853d429db
Teacher <grb.nikita@yandex.ru>
2023-12-12 21:03:49 +0500

    update from tests

--- virtual_background.py
+++ virtual_background.py
@@ -10,21 +10,17 @@ DESIRED_WIDTH = 480
 
 def process_image(image, background_image):
 
-    mp_selfie_segmentation = mp.solutions.selfie_segmentation
-
-    image = cv2.imread(image)
-    #background_image = cv2.imread(background_image)
 
     with mp.solutions.selfie_segmentation.SelfieSegmentation() as selfie_segmentation:
-        # img_as_np = np.frombuffer(image, dtype=np.uint8)
-        # image = cv2.imdecode(img_as_np, flags=1)
+        img_as_np = np.frombuffer(image, dtype=np.uint8)
+        image = cv2.imdecode(img_as_np, flags=1)
 
         # Convert the BGR image to RGB and process it with MediaPipe Selfie Segmentation.
         results = selfie_segmentation.process(cv2.cvtColor(image, cv2.COLOR_BGR2RGB))
 
-        #bg_as_np = np.frombuffer(background_image, dtype=np.uint8)
-        #background_image = cv2.imdecode(bg_as_np, flags=1)
-        background_image = cv2.imread(background_image)
+        bg_as_np = np.frombuffer(background_image, dtype=np.uint8)
+        background_image = cv2.imdecode(bg_as_np, flags=1)
+
 
         condition = np.stack((results.segmentation_mask,) * 3, axis=-1) > 0.1
         output_image = np.where(condition, image, background_image)
@@ -35,16 +31,28 @@ def process_image(image, background_image):
         else:
             img = cv2.resize(output_image, (math.floor(w / (h / DESIRED_HEIGHT)), DESIRED_HEIGHT))
 
-        cv2.imshow('img', img)
+    image_bytes = cv2.imencode('.jpg', img)[1].tobytes()
 
-        cv2.waitKey(0)
+    return image_bytes
 
-        # It is for removing/deleting created GUI window from screen
-        # and memory
-        cv2.destroyAllWindows()
 
-    # image_bytes = cv2.imencode('.jpg', img)[1].tobytes()
-    #
-    # return image_bytes
 
-process_image('datasets/cloun.jpg', 'backgrounds/back_test.jpg')
+# для теста вывод кртинки!!!
+
+data_img = cv2.imread('datasets/cloun.jpg')
+data_img = cv2.imencode('.jpg', data_img)[1].tobytes()
+
+back_img = cv2.imread('backgrounds/back_test.jpg')
+back_img = cv2.imencode('.jpg', back_img)[1].tobytes()
+
+
+img = process_image(data_img, back_img)
+
+
+
+nparr = np.frombuffer(img, np.byte)
+img2 = cv2.imdecode(nparr, cv2.IMREAD_ANYCOLOR)
+
+cv2.imshow('img', img2)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
